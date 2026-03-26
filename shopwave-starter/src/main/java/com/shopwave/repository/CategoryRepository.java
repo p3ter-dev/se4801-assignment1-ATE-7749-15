878273c8348192ab6bbc4bea7a4b2c758b018e3c
peter <peterkinfe548@gmail.com>
2026-03-26 19:06:06 +0300

    add JPA repository for Category persistence

---
+++ shopwave-starter/src/main/java/com/shopwave/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+// Name: Peter Kinfe
+// ID: ATE/7749/15
+package com.shopwave.repository;
+
+import com.shopwave.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {}
