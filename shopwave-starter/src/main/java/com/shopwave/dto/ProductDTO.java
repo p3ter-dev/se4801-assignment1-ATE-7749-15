b5c8978c85e42fe3436198b20cd41cf71f623a22
peter <peterkinfe548@gmail.com>
2026-03-26 18:50:20 +0300

    add ProductDTO for standardized API responses

---
+++ shopwave-starter/src/main/java/com/shopwave/dto/ProductDTO.java
@@ -0,0 +1,25 @@
+// Name: Peter Kinfe
+// ID: ATE/7749/15
+package com.shopwave.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+    private String categoryName;
+    private LocalDateTime createdAt;
+}
