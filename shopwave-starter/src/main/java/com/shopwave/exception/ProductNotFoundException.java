aba1aaf1d864a91874b1c39e1088fef8f4d298f9
peter <peterkinfe548@gmail.com>
2026-03-26 18:57:33 +0300

    add custom exception for missing product resources

---
+++ shopwave-starter/src/main/java/com/shopwave/exception/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+// Name: Peter Kinfe
+// ID: ATE/7749/15
+package com.shopwave.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(Long id) {
+        super("Product not found with id: " + id);
+    }
+}
